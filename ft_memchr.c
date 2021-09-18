893a995acf0da938bbb2098d1a0e219f6ea66b8d
SantanaC4 <eduardo.sanati@gmail.com>
2021-09-18 13:11:06 -0300

    Fix ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: edrodrig <edrodrig@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 15:55:55 by edrodrig          #+#    #+#             */
-/*   Updated: 2021/09/18 13:07:03 by edrodrig         ###   ########.fr       */
+/*   Updated: 2021/09/18 13:10:11 by edrodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	unsigned char	*p;
 
 	p = (unsigned char *)str;
-	while (*p && n--)
+	while (n--)
 		if (*p++ == (unsigned char)c)
 			return ((p - 1));
 	return (NULL);
