4194335419f1994d2c3963bb2fe7689f44b0035f
SantanaC4 <eduardo.sanati@gmail.com>
2021-09-18 14:00:00 -0300

    Fix ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: edrodrig <edrodrig@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 23:03:23 by edrodrig          #+#    #+#             */
-/*   Updated: 2021/09/18 13:49:55 by edrodrig         ###   ########.fr       */
+/*   Updated: 2021/09/18 13:58:44 by edrodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*n;
+	size_t new_len;
 
 	if (s == NULL)
 		return (NULL);
 	if ((unsigned int)ft_strlen(s) < start)
 		return (ft_strdup(""));
-	n = malloc((len + 1) * sizeof(char));
-	if (n == NULL)
+	new_len = ft_strlen(s + start);
+	if (new_len < len)
+		len = new_len;
+	if (!(n = malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	ft_memcpy(n, s + start, len);
 	n[len + 1] = '\0';
